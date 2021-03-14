b47ed1128d533e8af561cbcb531d9da637e83f35
Denis Garabajiu <denis1610@yahoo.com>
2021-03-14 18:06:33 +0100

    Comments + styling

--- Practical 4/Assignment8/nw1.m
+++ Practical 4/Assignment8/nw1.m
@@ -1,15 +1,19 @@
-in=fopen('input.txt'); 
+in=fopen('nw_test1.txt'); 
 s=fgetl (in);
 t=fgetl (in);
 fclose (in);
-len_s = length ( s ) ; 
+len_s = length(s) ; 
 len_t = length(t);
 
+% Test values from the assignment
 p = 0;
 q = 4;
 g = 5;
 
+% Allocating enough memory for the resulting matrix
 D = zeros(len_s + 1, len_t + 1);
+
+% Needleman-Wunsch Algorithm (9.1 in Reader)
 
 for i = 1 : (len_s + 1)
     D(i,1) = ((i - 1) * g);
@@ -28,10 +32,11 @@ for i = 2 : (len_s + 1)
     end
 end
 
-function x = w(a, b, p, q)
+% Function to compute the output value of the scoring matrix.
+function val = w(a, b, p, q)
     if (a == b) 
-        x = p;
+        val = p;
     else
-        x = q;
+        val = q;
     end
 end
